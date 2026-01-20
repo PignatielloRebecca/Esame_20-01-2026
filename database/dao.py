8183bf712e7ef28c13c27178d953a721723e46d2
PignatielloRebecca <s341105@studenti.polito.it>
2026-01-20 11:43:26 +0100

    soluzione

--- database/dao.py
+++ database/dao.py
@@ -20,3 +20,23 @@ class DAO:
         cursor.close()
         conn.close()
         return result
+
+    @staticmethod
+    def get_all_artists():
+
+        conn = DBConnect.get_connection()
+        result = []
+        cursor = conn.cursor(dictionary=True)
+        query = """
+                SELECT *
+                FROM artist a \
+                """
+        cursor.execute(query)
+        for row in cursor:
+            artist = Artist(id=row['id'], name=row['name'])
+            result.append(artist)
+        cursor.close()
+        conn.close()
+        return result
+
+
